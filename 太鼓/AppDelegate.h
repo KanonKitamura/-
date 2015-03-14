7364fcc3046bf9d91a50c2a1112948a3cc0a882f
KanonKitamura <kanonkitamura0405@gmail.com>
2015-03-14 10:14:32 +0900

    Initial Commit

---
+++ 太鼓/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  太鼓
+//
+//  Created by kanon kitamura on 2015/03/14.
+//  Copyright (c) 2015年 myname. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 太鼓/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  太鼓
+//
+//  Created by kanon kitamura on 2015/03/14.
+//  Copyright (c) 2015年 myname. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
